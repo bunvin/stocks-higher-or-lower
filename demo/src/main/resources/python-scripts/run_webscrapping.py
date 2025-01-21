7d445f4cb59756246d1d071c7be66a7d92d41079
NaamaL <bunvin@gmail.com>
2025-01-21 23:08:37 +0200

    BUG: run_webscrapping.py not working
    path exception

--- demo/src/main/resources/python-scripts/run_webscrapping.py
+++ demo/src/main/resources/python-scripts/run_webscrapping.py
@@ -3,10 +3,10 @@ import time
 import os
 
 def run_script():
-    os.system('python demo\\python-scripts\\webscraping-yahoo-finance-stocks-data.py')
+    os.system('python webscraping-yahoo-finance-stocks-data.py')
 
 # Schedule the script to run every day at midnight (00:00)
-schedule.every().day.at("23:00").do(run_script)
+schedule.every().day.at("23:06").do(run_script)
 
 while True:
     schedule.run_pending()
