dd34c1695f3d1dcad443977c7333a15d4e66a104
NaamaL <bunvin@gmail.com>
2025-01-21 13:19:14 +0200

    NEW: RestController, html+js files
    html showing stoks.json file data by activating the JavaScript

---
+++ demo/src/main/java/stonks_game/demo/StoksDataController.java
@@ -0,0 +1,28 @@
+package stonks_game.demo;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class StoksDataController {
+    @GetMapping("/stocks")
+    public String getStocksData() throws IOException {
+        String filePath = "python-scripts/stocks.json";
+        // load the file from classpath
+        ClassPathResource resource = new ClassPathResource(filePath);
+        if (resource.exists()) {
+            // Read file contents as a string
+            return new String(Files.readAllBytes(Paths.get(resource.getURI())));
+        } else {
+            return "File not found!";
+        }
+    }
+
+}
--- demo/src/main/resources/python-scripts/webscraping-yahoo-finance-stocks-data.py
+++ demo/src/main/resources/python-scripts/webscraping-yahoo-finance-stocks-data.py
@@ -1,8 +1,11 @@
 # %%
 import requests
 from bs4 import BeautifulSoup
+import json
 import pandas as pd
 import time
+from datetime import datetime
+import os
 
 # %%
 headers = {'user-agent':'Mozilla/5.0 \
@@ -38,13 +41,26 @@ for symbol in symbols:
         fin_streamer = soup.find('fin-streamer', attrs={'data-symbol': {company_symbol}, 'data-field': 'regularMarketVolume'})
         volume = fin_streamer.text.strip()
 
+        date_modified = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f"{company_name} - {date_modified}")
+
         #description
         soup = BeautifulSoup(description_page.text, 'html.parser')
         description = soup.find('section', {'data-testid': 'description'}).find('p').text
         description = description[:250]
 
-        x=[company_name,company_symbol,price,change,volume,url, description]
-        all.append(x)
+        company_data = {
+            "Company": company_name,
+            "Symbol": company_symbol,
+            "Price": price,
+            "Change": change,
+            "Volume": volume,
+            "URL": url,
+            "Description": description,
+            "Last_Updated": date_modified
+        }
+        
+        all.append(company_data)
 
     except AttributeError as e:
         print(f"Error occurred while scraping {symbol}: {e}")
@@ -55,9 +71,12 @@ for symbol in symbols:
 
 # %%
 #panda it
-column_names = ["Company", "Symbol", "Price", "Change", "Volume", "URL", "Description"]
+column_names = ["Company", "Symbol", "Price", "Change", "Volume", "URL", "Last_Updated" ,"Description" ]
 df = pd.DataFrame(all, columns=column_names)
-df.to_csv('demo\\python-scripts\\stocks.CSV')
+filepath = 'C:\\Fullstack_apps\\stocks-higher-or-lower\\demo\\src\\main\\resources\\python-scripts'
+json_file_path = os.path.join(filepath,'stocks.json')
 
-print("Data scraping completed and saved to 'stocks.csv'.")
+with open(json_file_path, 'w', encoding='utf-8') as json_file:
+    json.dump(all, json_file, ensure_ascii=False, indent=4)
 
+print("Data scraping completed and saved to 'stocks.json'.")
